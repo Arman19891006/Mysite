a69e7554e96aaf710d47acba15c8fd04c90122d2
Arman19891006 <arman.mohammadi198910@gmail.com>
2022-02-21 06:42:32 +0330

    created render html files
    added template folder and  put html files in it

--- mysite/views.py
+++ mysite/views.py
@@ -1,8 +1,2 @@
 from django.http import HttpResponse,JsonResponse
 
-def http_test(request):
-    return HttpResponse('Hi ArmanRizan')
-
-
-def json_test(request):
-    return JsonResponse({'Name':'ArmanRizan'})
