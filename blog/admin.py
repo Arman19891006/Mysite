f8a8ef3e9ffb738263943f68ac87ed41880465c8
Arman19891006 <arman.mohammadi198910@gmail.com>
2022-02-27 04:31:49 +0330

    create model post and blog
    create model post and blog and set it to admin page

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from blog.models import Post
 # Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = 'empty'
+    list_filter = ('status',)
+    #ordering = ('created_date',)
+    search_fields = ['title','content']
+    list_display =('title','counted_view','status','published_date','created_date')
+admin.site.register(Post,PostAdmin)
